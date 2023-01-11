7fa30d6bb37baec1d5949a1371e0e4259c860d0f
brayanospina2005 <ospinabrayan431@gmail.com>
2023-01-11 15:27:15 -0500

    section about, section tours, section services

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,11 @@ const closeMenu = document.querySelector(".close-menu")
 const overflow = document.querySelector(".body")
 menuOpen.addEventListener("click", () => {
   navMenu.classList.toggle("active");
-  overflow.classList.toggle("active");
+  overflow.classList.toggle("hidden");
   
 })
 closeMenu.addEventListener("click", () => {
   navMenu.classList.remove("active");
+  overflow.classList.remove("hidden");
+
 })
